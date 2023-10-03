63ba5af6a445b6c4bcb918ae682c4691573712a4
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2023-10-04 00:27:13 +0800

    create method for setting the safetybox details/values

--- src/bankaccountapp/Savings.java
+++ src/bankaccountapp/Savings.java
@@ -7,6 +7,9 @@ public class Savings extends Account{
     public Savings(String name, String SSN, double balance) {
         super(name, SSN, balance);
         accountNumber = "1" + accountNumber;
+        setSafetyBox();
+//        System.out.println(safetyDepositBox);
+//        System.out.println(safetyCode);
     }
 
     @Override
@@ -30,5 +33,11 @@ public class Savings extends Account{
         super.showInfo();
         System.out.println("ACCOUNT: SAVINGS \n " + "SafetyBox: " + safetyDepositBox + "\n SafetyCode: " + safetyCode );
         System.out.println("*****************************************************************************");
+    }
+
+    private void setSafetyBox(){
+        safetyDepositBox = (int) (Math.random()*Math.pow(10,3));
+        safetyCode = (int) (Math.random()*Math.pow(10,4));
+
     }
 }
