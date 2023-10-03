d93d9a48da4bed652381527e7a5e9a99e3e5ca6f
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2023-10-04 00:32:14 +0800

    create method for setting the debitcard data

--- src/bankaccountapp/Checking.java
+++ src/bankaccountapp/Checking.java
@@ -7,11 +7,9 @@ public class Checking extends Account{
     public Checking(String name, String SSN, double balance) {
         super(name, SSN, balance);
         accountNumber = "2" + accountNumber;
+        setDebitCard();
 
-        System.out.println("*****************************************************************************");
-        super.showInfo();
-        System.out.println("ACCOUNT: CHECKING \n " + "DebitCardNo.: " + debitCard + "\n PIN: " + PIN );
-        System.out.println("*****************************************************************************");
+
     }
 
     @Override
@@ -31,6 +29,14 @@ public class Checking extends Account{
 
     @Override
     public void showInfo() {
+        System.out.println("*****************************************************************************");
         super.showInfo();
+        System.out.println("ACCOUNT: CHECKING \n " + "DebitCardNo.: " + debitCard + "\n PIN: " + PIN );
+        System.out.println("*****************************************************************************");
+    }
+
+    private void setDebitCard(){
+        debitCard = String.valueOf((int) (Math.random()*Math.pow(10,3)));
+        PIN = (int) (Math.random()*Math.pow(10,4));
     }
 }
